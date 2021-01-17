b5b13f71de3c466279ba85948ec5bf54f8f6bbe4
fanyCaz <stepheniecaz@gmail.com>
2021-01-17 16:18:03 -0600

    gets the citation of a hardcoded book

--- src/bot.ts
+++ src/bot.ts
@@ -7,19 +7,32 @@ const axios = require('axios').default;
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let apaReply = function(response:any): string{
+	let authors:string = "not yet";
+	let yearPublished:string = response["publish_date"].slice(-4);
+	let title:string = response["title"];
+	let publisher:string = response["publishers"][0];
+	let reply:string = `${authors}.(${yearPublished}).${title}.${publisher}`;
+	return reply;
+}
+
 /*REQUEST TO OPEN LIBRARY*/
-let getBookInfo:() => any = function(): any {
-	let isbn: string = "9780140328721.json";
+async function getBookInfo(isbn:string, msg:any){
 	let openLibraryURL: string = "https://openlibrary.org/isbn/";
-	axios.get(openLibraryURL+isbn)
+	let reply:any = await axios.get(openLibraryURL+isbn+".json")
 		.then(function(response){
-			console.log(response);
-			
-		} )
+			console.log(response.data);
+			return response.data;
+		})
 		.catch(function(error){
 			console.log(error);
+			return "No he podido obtener el libro :tired_face:";
 		});
-}
+
+
+	let citation:string = apaReply(reply);
+	msg.reply(citation);
+};
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}`);
@@ -34,7 +47,8 @@ client.on('message', msg =>{
 		msg.reply('Polooo');
 	}
 	else if(message == 'getbook'){
-		getBookInfo();
+		let isbn: string = "9780140328721";
+		getBookInfo(isbn,msg);
 	}
 });
 

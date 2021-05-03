6c979e3c63b670f4be0f13b43619db9a5e055468
fanyCaz <stepheniecaz@gmail.com>
2021-05-02 20:39:59 -0500

    bug fix, separate functions

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ Bot that gets the APA reference of any book ... as long as it is available in th
 Now I want to include movies too... as long as they're available in this page: https://imdb-api.com/api
 
 ## How to : local
-1. Get in folder source: ```cd src```
-2. Make the TypeScript file in a JavaScript file: ```npx tsc bot.ts```
-3. Tell the bot to get something done: ```node bot.js```
+1. Get in folder source: ```cd apa-bot```
+2. Install dependencies: ```npm install```
+3. Tell the bot to get something done: ```yarn dev```
+or ```yarn watch```
 
 ### Also
 This is the first time using Node, TypeScript and Javascript(without wanting to end it all), so, this is as good as it gets.
+
+Edit:
+So I think its cleaner now, but we'll see
--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,57 @@
 "use strict";
 
-
 require('dotenv').config();
 
 const axios = require('axios').default;
 
+/*MAKE A MESSAGE*/
+function apaReply(response: any, author: string): string{
+	let year_published: string = (response["publish_date"]) ? response["publish_date"].slice(-4) : "xxx";
+	let title: string = (response["title"]) ? response["title"] : "xxx";
+	let publisher: string = (response["publishers"]) ? response["publishers"][0] : "xxx";
+	return `${author}(${year_published}).*${title}*.${publisher}`;
+}
+
+/*BOOK*/
+async function getAuthorInfo(author_key: string){
+	let openLibraryURL: string = "https://openlibrary.org/authors/";
+	let response: any = await axios.get(`${openLibraryURL}${author_key}.json`)
+						.then(function(res:any){
+							return res;
+						})
+						.catch(function(error: any){
+							return error;
+						});
+	if(response.status == 200){
+		return response.data["personal_name"];
+	}else{
+		"xxx";
+	}
+}
+
+async function getBookInfo(isbn:string, msg: any){ 
+	let openLibraryURL: string = "https://openlibrary.org/isbn/";
+	let response: any = await axios.get(`${openLibraryURL}${isbn}.json`)
+											.then(function(res: any){
+												return res;
+											})
+											.catch(function(error: any){
+												return error;
+											});
+	if(response.status == 200){
+		let author: string = "xxx";
+		if(response.data["authors"]){
+			let author_id: string = response.data["authors"][0].key.split('/')[2];
+			author = await getAuthorInfo(author_id);
+		}
+		let reference: string = apaReply(response.data, author);
+		msg.reply(reference);
+	}else{
+		msg.reply("No fue encontrado :confused:");
+	}
+}
+
+/*DISCORD*/
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -12,10 +59,27 @@ client.on('ready', () =>{
     console.log(`logged in ${client.user.tag}`);
 });
 
-client.on('message', (msg: string) =>{
-    let message: string = msg;
-    
-    console.log(message);
+client.on('message', (msg: any) =>{
+    let message: string = msg.content;
+
+		let command: string = message.split(' ')[0];
+		let args = message.split(' ');
+		switch(command){
+			case 'ping':
+				msg.reply('pong');
+				break;
+			case 'libro':
+			  if(args[1] != null){
+			 	  let isbn: string = args[1];
+					if(isbn.length == 13 || isbn.length == 10){
+						getBookInfo(isbn, msg);
+					}else{
+						msg.reply("Pasa un isbn porfavor :upside_down_face:")
+					}
+				}
+			break;
+		}
+		
 });
 
 client.login(process.env.TOKEN);
